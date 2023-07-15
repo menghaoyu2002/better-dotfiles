6f5561b6192ebe7ccc742f0ff07118b968ff9eb4
Menghao Yu <menghaoyu2002@gmail.com>
2023-07-15 17:45:09 -0400

    add powermenu and datemenu

--- eww/scripts/init.sh
+++ eww/scripts/init.sh
@@ -3,14 +3,17 @@
 eww daemon
 
 monitor=$(hyprctl monitors -j | jaq 'reverse' | jaq '.[0].id')    
+eww update mainmonitor=${monitor}
 eww open bar${monitor}
 
 socat -u UNIX-CONNECT:/tmp/hypr/"$HYPRLAND_INSTANCE_SIGNATURE"/.socket2.sock - | while read -r line; do
     case ${line%>>*} in
     "monitorremoved")
+    eww update mainmonitor=0
     eww open bar0
     ;;
     "monitoradded")
+    eww update mainmonitor=1
     eww close bar0
     sleep 1
     eww open bar1
