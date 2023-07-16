9993f256ff165eb88993afbd9ad98fdec0a6684e
Menghao Yu <menghaoyu2002@gmail.com>
2023-07-16 14:02:51 -0400

    add spotify menu

--- eww/scripts/spotify.sh
+++ eww/scripts/spotify.sh
@@ -1,8 +1,35 @@
 #!/usr/bin/env bash
 
+coverart=$(cat ./coverart)
+
 echo "{"
-playerctl -p spotify metadata | awk '{print $2 " " $3}' | while read -r line; do 
-echo ${line:6} | awk '{print "\""$1"\"" ": " "\""$2"\""","}'
+playerctl -p spotify metadata | while read -r line; do 
+
+line=${line#*:} 
+value=$(echo ${line#*' '} | xargs -0 | tr -d '"')
+key=$(echo ${line%"$value"} | xargs)
+
+if [[ $key = "artUrl" && $coverart != $value ]]; then
+    curl -sS $value > ./coverart.png
+    coverart=$value
+    echo $coverart > ./coverart
+fi
+
+echo '"'$key'"':  '"'$value'"'','
+
 done
-echo '"status" : "'"$(playerctl -p spotify status)"'"'
+
+echo '"volume": "'"$(playerctl -p spotify volume)"'",'
+
+echo '"status": "'"$(playerctl -p spotify status)"'",'
+echo '"coverart": "./coverart.png",'
+
+echo '"shuffle": "'"$(playerctl -p spotify shuffle)"'",'
+echo '"loop": "'"$(playerctl -p spotify loop)"'",'
+length=$(playerctl -p spotify metadata --format "{{ duration(mpris:length)}}")
+echo '"length_str": "'"$length"'",'
+echo '"length_seconds": "'"$(echo $length | awk -F ":" '{ print $1 * 60 + $2}')"'",'
+
+echo '"position_seconds": "'"$(playerctl -p spotify position)"'",'
+echo '"position_str": "'"$(playerctl -p spotify position --format "{{ duration(position) }}")"'"'
 echo "}"
